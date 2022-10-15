98fb7acfe5e743a6f952bbb00e24036df0d565f4
ZaraMelikyan <zaramelikyan1@gmail.com>
2022-10-15 12:08:24 +0300

    hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,3 +2,53 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const itemsContainerRef = document.querySelector('.gallery');
+const itemMarkup = createGalleryItemMarkup(galleryItems);
+itemsContainerRef.insertAdjacentHTML('beforeend', itemMarkup);
+
+function createGalleryItemMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt= "${description}"
+    />
+  </a>
+</div> `;
+    })
+    .join('');
+}
+
+itemsContainerRef.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  const originalImg = event.target.dataset.source;
+
+  showModal(originalImg);
+}
+
+function showModal(imgUrl) {
+  const modal = basicLightbox.create(`
+    <img src="${imgUrl}" width="800" height="600">
+`);
+
+  modal.show(() => window.addEventListener('keydown', closeModalByEsc));
+
+  function closeModalByEsc(event) {
+    if (event.code === 'Escape') {
+      modal.close(() => window.removeEventListener('keydown', closeModalByEsc));
+    }
+  }
+}
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -2,3 +2,38 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+console.log(galleryItems);
+
+const itemsContainerRef = document.querySelector('.gallery');
+const itemMarkup = createGalleryItemMarkup(galleryItems);
+itemsContainerRef.insertAdjacentHTML('beforeend', itemMarkup);
+
+function createGalleryItemMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</div> `;
+    })
+    .join('');
+}
+
+itemsContainerRef.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  let gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    overlayOpacity: 0.8,
+  });
+}
